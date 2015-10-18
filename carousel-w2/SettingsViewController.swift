d30dc5497641461c7402186063ca1d4d2c75c96a
Ciro Verdi <ciroverdi@gmail.com>
2015-10-18 15:02:59 -0200

    Creating Settings

--- carousel-w2/SettingsViewController.swift
+++ carousel-w2/SettingsViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIScrollViewDelegate, UIActionSheetDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var signOutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
+        scrollView.contentSize = imageView.image!.size
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func goBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+        navigationController?.popToRootViewControllerAnimated(true)
+
+    }
+    
+    @IBAction func signOut(sender: AnyObject) {
+        UIActionSheet(title: "Are you sure you want to Sign Out?", delegate: self, cancelButtonTitle: "Cancel", destructiveButtonTitle: "Sign Out").showInView(view)
+        
+    }
+    
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+        if buttonIndex == 0 {
+            performSegueWithIdentifier("signOutSegue", sender: nil)
+        }
     }
 }
