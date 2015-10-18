35c5717c2dd9f8eb636defba3b23dcb42be1229a
Ciro Verdi <ciroverdi@gmail.com>
2015-10-18 15:46:58 -0200

    Creating README and animamted GIF

---
+++ README.md
@@ -0,0 +1,19 @@
+# carousel-w2
+
+This is an iOS demo appliction to leverage views, view properties, and events to create a high fidelity prototype that is difficult to distinguish from a production app.
+
+Time spent: 9 hours spent in total
+
+Completed user stories:
+
+ Required: Sign In
+ Optional: The scrollview should only be scrollable when the keyboard is shown
+ Optional: Scrolling down a specific amount should:
+ Required: Tutorial Screens
+ Required: Image Timeline
+ Required: Conversations
+ Required: Settings
+ Optional: Tapping the Sign Out button presents a UIAlertController that gives options to Sign Out or Cancel
+
+ ![Video Walkthrough](carousel-w2.gif)
+ 
--- carousel-w2/IntroViewController.swift
+++ carousel-w2/IntroViewController.swift
@@ -32,33 +32,6 @@ class IntroViewController: UIViewController, UIScrollViewDelegate {
         super.didReceiveMemoryWarning()
     }
     
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        // This method is called as the user scrolls
-//        view.transform = CGAffineTransformMakeScale(2, 2)
-//        view.transform = CGAffineTransformRotate(view.transform, CGFloat(45 * M_PI / 180))
-    }
-    
-    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
-        
-    }
-    
-    func scrollViewDidEndDragging(scrollView: UIScrollView,
-        willDecelerate decelerate: Bool) {
-            // This method is called right as the user lifts their finger
-            
-    }
-    
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        // This method is called when the scrollview finally stops scrolling.
-//        view.transform = CGAffineTransformIdentity
-
-        // Get the current page based on the scroll offset
-        let page : Int = Int(round(scrollView.contentOffset.x / 320))
-        
-        // Set the current page, so the dots will update
-        pageControl.currentPage = page
-    }
-    
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return imageView
     }
